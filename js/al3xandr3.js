c828fb974027abf9898434c9dec254f32861bb98
al3xandr3 <al3xandr3@gmail.com>
2010-04-14 12:34:31 +0300

    about page, and couple of posts

---
+++ js/al3xandr3.js
@@ -0,0 +1,12 @@
+
+$(function(){
+  //Show and Hide the dates in posts in index page
+  /*
+  $("dd").hide();
+  $(".post-link").hover(function() {
+    $(this).parent().next("dd").show();
+  }, function() {
+    $(this).parent().next("dd").hide();
+  });
+  */
+});
