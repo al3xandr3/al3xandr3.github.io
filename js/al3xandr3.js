070e798955cd96a744e17b25408958330a5dd9fc
al3xandr3 <al3xandr3@gmail.com>
2010-04-17 14:21:51 +0300

    adding disqus back in

--- js/al3xandr3.js
+++ js/al3xandr3.js
@@ -1,16 +1,5 @@
 
 $(function(){
-
   //centering images in posts
   $("#post p img").parent().css( "text-align","center"); 
-
-  //Show and Hide the dates in posts in index page
-  /*
-  $("dd").hide();
-  $(".post-link").hover(function() {
-    $(this).parent().next("dd").show();
-  }, function() {
-    $(this).parent().next("dd").hide();
-  });
-  */
 });
