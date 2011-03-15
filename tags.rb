5b0109bb437bfa1c8f64d411d86c3b9404b993b2
al3xandr3 <al3xandr3@gmail.com>
2011-03-15 11:27:04 +0200

    adding dates to categories and bigger size

--- tags.rb
+++ tags.rb
@@ -17,14 +17,18 @@ title: Posts on "#{category}"
     <h1 id="#{category}">Posts on "#{category}"</h1>
 HTML
   
-  html << '<ul class="posts">'
+  html << '<div id="category-list"><ul class="posts">'
   posts.each do |post|
     post_data = post.to_liquid   
     html << <<-HTML
-        <li><a href="#{post.url}">#{post_data['title']}</a></li>
+
+        <li>
+          <a href="#{post.url}">#{post_data['title']}</a>
+          <span id="post-date" class="alt">#{ post.date.strftime('%d %b %Y') }</span>
+       </li>
       HTML
   end
-  html << '</ul>'
+  html << '</div></ul>'
   
   File.open("/my/al3xandr3.github.com/tags/#{category}.html", 'w+') do |file|
     file.puts html
