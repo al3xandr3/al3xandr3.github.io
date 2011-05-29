b94aa8a1d945f54b16235d3972a6acf60033e6b3
al3xandr3 <al3xandr3@gmail.com>
2011-05-29 10:50:22 +0300

    re-design

--- cloud.rb
+++ cloud.rb
@@ -8,16 +8,14 @@ options = Jekyll.configuration({'source' => "/my/al3xandr3.github.com/"})
 site = Jekyll::Site.new(options)
 site.read_posts('')
 
-html = "<h3 id=\"title-cloud\">tags</h3>"
-html << "<div id=\"cloud\">"
+html = ""
 
 site.categories.sort.each do |category, posts|
   s = posts.count
-  font_size = 10 + (s*1.5);
+  font_size = 10 + (s*1.2);
   html << "<a href=\"/tags/#{category}.html\" id=\"tag\" title=\"Postings tagged #{category}\" style=\"font-size: #{font_size}px; line-height:#{font_size}px\">#{category}</a> "
 end
 
-html << "</div>"
 File.open('/my/al3xandr3.github.com/_includes/cloud.html', 'w+') do |file|
   file.puts html
 end
