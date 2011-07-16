a7cfd5548bf233be63460df1ffebe414148be876
al3xandr3 <al3xandr3@gmail.com>
2011-07-16 22:36:36 +0300

    updates

--- pre-process.rb
+++ pre-process.rb
@@ -17,29 +17,15 @@ def process_post(post, layout)
   cats = (doc/'category').remove
   cats = cats.inner_html.split(' ').map { |i| i.downcase }
 
+  # Extract intro
+  intro = (doc/'p')[0]
+  intro = intro.inner_html
+
   # Extract h2 title
   h1 = (doc/'h1.title')
   #cleans the title from the post itself, so i can use as meta only
   (doc/'h1.title').remove
-  # Extract timestamp
-  #timestamp = (doc/'span.timestamp-wrapper').remove
-  #if timestamp
-  #  br = (doc/'div#text-1 p br:first')
-  #  br.remove unless br.nil? # Remove <br /> right after timestamp-wrapper
-  #  # I use europen date format, must do some conversion to make Time.parse work
-  #  t = timestamp.search('span.timestamp').inner_html
-  #  unless t.nil? or t.empty?
-  #    t = t.split # 03/06/09 Wed 15:00
-  #    tt = t[0].split('/') # 03/06/09
-  #    tt = [tt[2], tt[1], tt[0]].join('/') # Euro -> US date
-  #    if t[2] # If the timestamp includes time
-  #      date = Time.parse("#{tt} #{t[2]} +2") # +2 is my local time zone offset
-  #    else # only date
-  #      date = Time.parse("#{tt} +2")
-  #    end
-  #  end
-  #end
-
+ 
   # Extract the top outline
   post = doc.search('div#content').inner_html
   
@@ -51,6 +37,7 @@ def process_post(post, layout)
   meta['layout'] = layout
   meta['title'] = h1.inner_html.gsub('&nbsp;', '').strip # insert h1 title
   meta['categories'] = cats unless cats.empty?
+  meta['intro'] = intro unless intro.empty?
   #meta['date'] = date if date
 
   meta = meta.to_yaml + "---\n\n"
