adb1747ac784b8c7ed9f81bcc694913886068384
Alexandre Martins <al3xandr3@gmail.com>
2012-04-05 17:11:53 +0300

    tag cloud update

--- quotes_cloud.r
+++ quotes_cloud.r
@@ -1,8 +1,8 @@
 source("~/Dropbox/my/dev/r/rprofile")
 
-al3x.load()
+a.load()
 f = readLines('./quotes_temp.txt')
-d = al3x.word.freq(data.frame(f))
+d = a.word.freq(data.frame(f))
 png('./img/quotes_cloud.png', width=500,height=500)
 wordcloud(d$word, d$freq, min.freq=2, max.words=50, random.order=FALSE, colors=brewer.pal(8,"Dark2"))
 dev.off()
