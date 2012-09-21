aa610a4ee50d316b7c7efe20aebcb18dde7f0ce5
Alexandre Martins <al3xandr3@gmail.com>
2012-09-21 15:40:35 +0300

    cloud update

--- quotes_cloud.r
+++ quotes_cloud.r
@@ -12,7 +12,7 @@ f$day2 = factor(f$day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", 
 ggplot(f, aes(day2)) + geom_histogram(aes_string(y="..count..",fill="..count.."), alpha=0.6) + 
                                       xlab(NULL) + ylab(NULL) + opts(legend.position = "none") +
                                      opts(panel.border = theme_blank(), panel.background = theme_blank(),
-                                    axis.text.x=theme_text(size=12,angle=65, hjust=0.5), strip.background = theme_blank())
+                                    axis.text.x=theme_text(size=12,angle=65, hjust=0.5), strip.background = theme_blank()) +
 dev.off()
 
 # wordcloud
@@ -24,6 +24,6 @@ wordcloud(d$word
           ,scale=c(4,.5)
           ,max.words=50
           ,random.order=FALSE
-          ,colors=brewer.pal(9, "BuGn")
+          ,colors=brewer.pal(9,"Blues")
           )
 dev.off()
