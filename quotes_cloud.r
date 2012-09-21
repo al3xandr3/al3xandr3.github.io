087db61d13258ef3de1558568813913dfc86a598
Alexandre Martins <al3xandr3@gmail.com>
2012-09-21 18:05:09 +0300

    cloud

--- quotes_cloud.r
+++ quotes_cloud.r
@@ -10,18 +10,17 @@ f$day = weekdays(as.Date(f$date))
 png('./img/quotes_frequency.png', width=300,height=200)
 f$day2 = factor(f$day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
 ggplot(f, aes(day2)) + geom_histogram(aes_string(y="..count..",fill="..count.."), alpha=0.6) + 
-                                      xlab(NULL) + ylab(NULL) + opts(legend.position = "none") +
-                                     opts(panel.border = theme_blank(), panel.background = theme_blank(),
-                                    axis.text.x=theme_text(size=12,angle=65, hjust=0.5), strip.background = theme_blank())
+                                      xlab(NULL) + ylab(NULL) + theme(legend.position = "none") +
+                                      theme(panel.border = element_blank(), panel.background = element_blank(),
+                                      axis.text.x=element_text(size=12,angle=65, hjust=1), strip.background = element_blank())
 dev.off()
 
 # wordcloud
 d = a.word.freq(f$content)
-png('./img/quotes_cloud.png', width=500,height=500)
+png('./img/quotes_cloud.png', width=600, height=500)
 wordcloud(d$word
           ,d$freq
           ,min.freq=2
-          ,scale=c(4,.5)
           ,max.words=50
           ,random.order=FALSE
           ,colors=brewer.pal(9,"Blues")
